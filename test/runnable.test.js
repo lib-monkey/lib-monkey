22842d4276de9879a32b788d83bd412001d14ae5
Dmitry Dodzin <d.dodzin@gmail.com>
2017-10-01 19:40:01 +0300

    Adding a basic test env and a couple of tests for FunctionRunner and Runable

---
+++ test/runnable.test.js
@@ -0,0 +1,130 @@
+
+import sinon from 'sinon';
+import chai, { expect, assert } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+chai.should();
+
+import Joker from '../lib/common/joker';
+import Runable from '../lib/common/runable';
+
+describe('Runnable', () => {
+
+  it('Initialisation', () =>{
+    expect(() => new Runable(sinon.spy())).to.not.throw();    
+    expect(() => new Runable(sinon.spy(), [1, 2, 3])).to.not.throw();  
+    expect(() => new Runable(sinon.spy(), [1, 2, 3], this)).to.not.throw();
+  });
+
+  it('Calling', () => {
+    let fn = sinon.spy();
+    let params = [1, 2, 3];
+    let runable = new Runable(fn, params);
+
+    runable.call().should.eventually.be.fulfilled
+      .then(() => assert(fn.calledOnce));
+  });
+
+  it('Sync', () => {
+    let params = [11];
+    let fn = (value) => {
+      expect(value).to.equal(params[0]);
+    };
+
+    let runable = new Runable(fn, params);
+    return runable.call().should.eventually.be.fulfilled;
+  });
+
+  it('Sync fail', () => {
+    let params = [11];
+    let fn = (value) => {
+      expect(value).to.equal("something else");
+    };
+
+    let runable = new Runable(fn, params);
+    return runable.call().should.eventually.be.rejected;
+  });
+
+  it('Promise Async', () => {
+    let fn = () => 
+      new Promise((resolve, reject) => {
+        setTimeout(() => resolve());
+      });
+
+    let runable = new Runable(fn);
+    return runable.call().should.eventually.be.fulfilled;
+  });
+
+  it('Promise Async Fail', () => {
+    let fn = () => 
+      new Promise((resolve, reject) => {
+        setTimeout(() => reject(new Error('Some Error')));
+      });
+
+    let runable = new Runable(fn);
+    return runable.call().should.eventually.be.rejected;
+  });
+
+  it('Callback Async', () => {
+    let fn = (done) => {
+      setTimeout(() => done());
+    };
+
+    let runable = new Runable(fn);
+    return runable.call().should.eventually.be.fulfilled;
+  });
+
+  it('Callback Sync Fail', () => {
+    let fn = (done) => { 
+      throw new Error('Some assertion error');
+    };
+
+    let runable = new Runable(fn);
+    return runable.call().should.eventually.be.rejected;
+  });
+
+  it('Callback Async Fail', () => {
+    let fn = (done) => {
+      setTimeout(() => done(new Error('Some assertion error')));
+    };
+
+    let runable = new Runable(fn);
+    return runable.call().should.eventually.be.rejected;
+  });
+
+  it('Extract Joker Params', () => {
+    let fakeChance = { bool: () => false };
+    let joker_value = 'Some Value';
+    let joker = new Joker(fakeChance);
+    sinon.stub(joker, 'getValue').returns(joker_value);
+
+    let params = [joker];
+    let fn = sinon.spy();
+
+    let runable = new Runable(fn, params);
+    return runable.call()
+      .should.eventually.be.fulfilled
+      .then(() => assert(fn.calledWith(joker_value), 'Joker value wasn\'t called'));
+  });
+
+  it('Internal Error', () => {
+    let fakeChance = { bool: () => false };
+    let joker_value = 'Some Value';
+    let joker = new Joker(fakeChance);
+    sinon.stub(joker, 'getValue').throws(new Error('Hey'));
+
+    let params = [joker];
+
+    let runable = new Runable(() => {}, params);
+
+    let extend = { hey: 'ho' };
+
+    return runable.call(extend)
+      .should.eventually.be.rejected
+      .then(res => {
+        expect(res).to.nested.include({'err.message': 'Hey'});
+        expect(res.extend).to.deep.equal(extend);
+      });
+  });
+
+});
