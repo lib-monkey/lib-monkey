ec296e78ea7b56ca27428dfe79df844194b78a3e
Dmitry Dodzin <d.dodzin@gmail.com>
2017-10-26 20:11:48 +0300

    Adding more tests to piped runner

--- test/piped.runner.test.js
+++ test/piped.runner.test.js
@@ -22,7 +22,7 @@ describe('PipedRunner', () => {
   });
 
   it('Piped Execution', () => {
-    let runable = { call: sinon.stub().returns(new Promise((resolve) => resolve({ value: null }))) };
+    let runable = { call: sinon.stub().returns(Promise.resolve({ value: null })) };
 
     let pnr = new PipedRunner(runable, runable);
 
@@ -31,12 +31,61 @@ describe('PipedRunner', () => {
   });
 
   it('Parallel Piped Execution', () => {
-    let runable = { call: sinon.stub().returns(new Promise((resolve) => resolve({ value: null }))) };
+    let runable = { call: sinon.stub().returns(Promise.resolve({ value: null })) };
 
     let pnr = new PipedRunner(runable, runable);
 
     return pnr.exec(200, 10).should.eventually.be.fulfilled
       .then(() => expect(runable.call.callCount).to.equal(400));
+  });
+
+  it('Piped Params', () => {
+    let initialValue = '12';
+
+    let runable = { call: sinon.stub().returns(Promise.resolve({ value: initialValue })) };
+    let runable2 = { call: sinon.stub().returns(Promise.resolve({ value: null })) };
+
+    let pnr = new PipedRunner(runable, runable2);
+
+    return pnr.exec(1).should.eventually.be.fulfilled
+      .then(() => assert(runable2.call.calledWith(initialValue), 'Value not passed to second runnable'));
+  });
+
+  it('Falsable Piped Params', () => {
+    let values = [false, 0, null];
+
+    return Promise.all(values.map(val => {
+      let runable = { call: sinon.stub().returns(Promise.resolve({ value: val })) };
+      let runable2 = { call: sinon.stub().returns(Promise.resolve({ value: null })) };
+
+      let pnr = new PipedRunner(runable, runable2);
+
+      return pnr.exec(1).should.eventually.be.fulfilled
+        .then(() => assert(runable2.call.calledWith(val), `Value not passed to second runnable for ${val}`));
+    }));
+  });
+
+  it('Piped Error', () => {
+    let rejectVaue = {
+      err: new Error('Some Value'),
+      params: [1, 2, 3],
+      extend: { index: 0 }
+    };
+
+    let runable = { 
+      call: sinon.stub().returns(
+        Promise.reject(rejectVaue)
+      ) 
+    };
+
+    let pnr = new PipedRunner(runable);
+
+    return pnr.exec(1).should.eventually.be.rejected
+      .then(err => {
+        expect(err.message).to.have.string(`Path: PipedRunner(${rejectVaue.extend.depth}:${rejectVaue.extend.index})`);
+        expect(err.message).to.have.string(`Params: [${rejectVaue.params.join(', ')}]`);
+      });
+
   });
 
 });
