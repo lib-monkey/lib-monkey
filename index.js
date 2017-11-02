a482482518d569156b958f7073fcaf472295daad
Dmitry Dodzin <d.dodzin@gmail.com>
2017-11-02 17:12:51 +0200

    Starting adding the docs

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ let lm = { };
 monkey.JokerCreator.create(lm);
 monkey.RunnerCreator.create(lm);
 
+/**
+ * Lib-Monky.
+ * @module lib-monkey
+ * @autor Team of Monkeys
+ * @property {BooleanJoker} bool - return instance of the BooleanJoker
+ * @property {CharJoker} char - return instance of the CharJoker
+ * @property {FloatJoker} float - return instance of the FloatJoker
+ * @property {LetterJoker} letter - return instance of the LetterJoker
+ * @property {NaturalJoker} natural - return instance of the NaturalJoker
+ * @property {StringJoker} string - return instance of the StringJoker
+ */
 module.exports = lm;
