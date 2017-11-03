5f7de5bafc6602ecb65907410cba0c0748d23e31
Dmitry Dodzin <d.dodzin@gmail.com>
2017-11-03 19:12:40 +0200

    Adding more docs to changes

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ monkey.RunnerCreator.create(lm);
  * Lib-Monky.
  * @module lib-monkey
  * @autor Team of Monkeys
- * @property {BooleanJoker} bool - return instance of the BooleanJoker
- * @property {CharJoker} char - return instance of the CharJoker
- * @property {FloatJoker} float - return instance of the FloatJoker
- * @property {LetterJoker} letter - return instance of the LetterJoker
- * @property {NaturalJoker} natural - return instance of the NaturalJoker
- * @property {StringJoker} string - return instance of the StringJoker
+ * @property {Jokers.BooleanJoker} bool - return instance of the BooleanJoker
+ * @property {Jokers.CharJoker} char - return instance of the CharJoker
+ * @property {Jokers.FloatJoker} float - return instance of the FloatJoker
+ * @property {Jokers.LetterJoker} letter - return instance of the LetterJoker
+ * @property {Jokers.NaturalJoker} natural - return instance of the NaturalJoker
+ * @property {Jokers.StringJoker} string - return instance of the StringJoker
+ * @property {Jokers.ParagraphJoker} string - return instance of the ParagraphJoker
+ * @property {Jokers.SentenceJoker} string - return instance of the SentenceJoker
  */
 module.exports = lm;
