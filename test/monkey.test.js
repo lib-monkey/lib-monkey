41459c06dd27eac18cce55a04e8e7bc9d7fafa41
Dmitry Dodzin <d.dodzin@gmail.com>
2017-11-04 18:34:08 +0200

    Adding test for initialisation of the randomiser

---
+++ test/monkey.test.js
@@ -0,0 +1,49 @@
+
+import sinon from 'sinon';
+import { expect, assert } from 'chai';
+
+import Chance from 'chance';
+
+const __requireUncached = module => {
+  delete require.cache[require.resolve(module)];
+  return require(module);
+};
+
+describe('Monkey', () => {
+
+  before(() => {
+    process.env.LIB_MONKEY_ART = 0;
+  });
+
+  it('Regular Initialisation', () => {
+
+    const monkey = __requireUncached('../lib/monkey');
+
+    expect(monkey).to.have.property('randomiser');
+    expect(monkey).to.have.property('JokerCreator');
+    expect(monkey).to.have.property('RunnerCreator');
+  });
+
+  it('Random Seed for Randmosizer', () => {
+
+    delete process.env.LIB_MONKEY_SEED;
+
+    const monkey = __requireUncached('../lib/monkey');
+
+    expect(monkey.randomiser).to.be.instanceOf(Chance);
+  });
+
+  it('Ready Seed for Randmosizer', () => {
+
+    let seed = '123123';
+
+    let testRandomiser = new Chance(seed);
+
+    process.env.LIB_MONKEY_SEED = seed;
+
+    const monkey = __requireUncached('../lib/monkey');
+
+    expect(monkey.randomiser.integer()).to.be.equal(testRandomiser.integer());
+  });
+
+});
